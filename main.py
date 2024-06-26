164b0cc9300811b30bd0c9dc0aaf80b820d3947d
ThiagoDuraes <thiagosantos881@hotmail.com>
2024-06-25 22:20:29 -0300

    csv_para_analisar.
    Analisando novo csv

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ pd.options.display.max_rows = 9999
 pd.options.display.max_columns = 15
 df = pd.read_csv('primates_dataset.csv')
 print(df.info())
-
-df.dropna(inplace = True)
-
-print(df.info())
 print(df.to_string)
+
+new_csv = df.dropna()
+new_csv.to_csv("novo_csv_analisar", sep = ';', index=False, encoding='utf-8')
+
+print(new_csv.info())
+print(new_csv.to_string)
 #print(novo_csv.info())
