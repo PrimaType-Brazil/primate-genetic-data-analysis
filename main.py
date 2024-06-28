7b4f8325d5c0b63bef15ec3618b19faf9a017ebb
ThiagoDuraes <thiagosantos881@hotmail.com>
2024-06-28 18:18:31 -0300

    att_28/06
    longe de acabar?

--- main.py
+++ main.py
@@ -4,12 +4,21 @@ import pandas as pd
 pd.options.display.max_rows = 9999
 pd.options.display.max_columns = 15
 df = pd.read_csv('primates_dataset.csv')
-print(df.info())
-print(df.to_string)
+#print(df.info())
+#print(df.to_string)
 
-new_csv = df.dropna()
-new_csv.to_csv("novo_csv_analisar", sep = ';', index=False, encoding='utf-8')
+#new_csv = df.dropna()
+#new_csv.to_csv("novo_csv_analisar", sep = ';', index=False, encoding='utf-8')
 
-print(new_csv.info())
-print(new_csv.to_string)
+
+dfgenetic = df.loc[df['genetic_variation']!=0,['genetic_variation','species_name']]
+q1 = df['genetic_variation'].quantile(0.25)
+q2 = df['genetic_variation'].quantile(0.75)
+print(q1)
+print(q2)
+#print(new_csv.info())
+#print(new_csv.to_string)
+print(dfgenetic.describe)
+print(dfgenetic.info())
 #print(novo_csv.info())
+#64 14 5
